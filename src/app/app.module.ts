e08da9e4941a033fb3b6b7937e17fb42001cb21d
Diego <d.rojas8@duocuc.cl>
2023-10-17 22:33:12 -0300

    Prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,11 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/recuperar/recuperar.page.ts
+++ src/app/recuperar/recuperar.page.ts
@@ -7,22 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./recuperar.page.scss'],
 })
 export class RecuperarPage {
-  showSpinner = false;
-  loadingPercentage = 0;
-
+  
   constructor(private router: Router) { }
 
   iniciarCarga() {
-    this.showSpinner = true;
-    const interval = setInterval(() => {
-      this.loadingPercentage += 10;
-      if (this.loadingPercentage >= 100) {
-        clearInterval(interval);
-        this.showSpinner = false;
+   
+    
         this.router.navigate(['/login']);
       }
-    }, 200);
-  }
+   
+  
 }
 
 
