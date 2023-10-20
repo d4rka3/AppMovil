be76d626a9e24ae4f28b240b3059c6379f663cc3
Diego <d.rojas8@duocuc.cl>
2023-10-20 01:23:12 -0300

    servicio django

--- src/app/service/django.service.ts
+++ src/app/service/django.service.ts
@@ -6,16 +6,14 @@ import { retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
-  apiURL = 'http://127.0.0.1:8000/';
+export class DjangoService {
+
+  apiURL = 'http://127.0.0.1:8000/api';
 
   constructor(private http: HttpClient) { }
 
   getNameUser(): Observable<any> {
-    return this.http.get(this.apiURL + 'api/lista_users/')
+    return this.http.get(this.apiURL + '/lista_users/')
       .pipe(retry(3));
   }
 }
- 
-
-
